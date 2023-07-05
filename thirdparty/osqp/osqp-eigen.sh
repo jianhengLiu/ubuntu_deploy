4c2549e074a4b750e9209ef188957e05cc8b86c7
Chris Liu <943678231@qq.com>
2023-07-05 18:35:35 +0800

    ✨ feat(osqp):

---
+++ thirdparty/osqp/osqp-eigen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the directory name
+dir_name="$HOME/Softwares"
+
+# Check if the directory exists
+if [ ! -d "$dir_name" ]; then
+  # If it doesn't exist, create it
+  mkdir "$dir_name"
+  echo "Created directory $dir_name"
+else
+  echo "Directory $dir_name already exists"
+fi
+
+cd $dir_name
+git clone https://github.com/robotology/osqp-eigen.git
+cd osqp-eigen
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
