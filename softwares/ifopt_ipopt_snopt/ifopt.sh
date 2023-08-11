93a5cbcf17ad433768993836d13879305b987459
Chris Liu <943678231@qq.com>
2023-08-11 10:53:03 +0800

    ✨ feat(snopt):

--- softwares/ifopt_ipopt_snopt/ifopt.sh
+++ softwares/ifopt_ipopt_snopt/ifopt.sh
@@ -14,7 +14,14 @@ fi
 cd $dir_name
 git clone https://github.com/ethz-adrl/ifopt.git && cd ifopt
 mkdir build && cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
+## to build snopt solver: https://github.com/castanhas98/install-snopt-tutorial
+# export SNOPT_DIR=dir_name/snopt-interface
+# cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SNOPT=ON ..
+### modify code/.cmake
+# FindSNOPT.cmake: set(snopt_v76 FALSE) -> set(SNOPT_v76 TRUE)
+# snopt_adapter.cc: setUserFun(&SnoptAdapter::ObjectiveAndConstraintFct); -> this->initialize("print_file.out",1);
+###
 make
 sudo make install # copies files in this folder to /usr/local/*
 # sudo xargs rm < install_manifest.txt # in case you want to uninstall the above
