0ed34703d4b855046d9800cbd04d6d24021b6880
chris_wsl <943678231@qq.com>
2023-09-08 11:47:28 +0800

    ✨ feat(latex):

---
+++ softwares/latex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sudo apt-get install texlive-latex-base #basic latex oackages
+# sudo apt-get install latex-cjk-all #letex chinese language package
+# sudo apt-get install texlive-latex-extra #LaTeX supplementary packages
+# sudo apt-get install texlive-xetex #latex编译器
+# sudo apt-get install texlive-publishers 
+# sudo apt-get install texlive-science
+sudo apt install texlive-latex-base latex-cjk-all texlive-latex-extra texlive-xetex texlive-publishers texlive-science texlive-font-utils
